113f3559f782ee5fef10252183a69ddf10bdbc2d
OH <ohmydaniel@gmail.com>
2020-08-26 00:39:25 +0900

    Registration

--- client/src/component/auth/Register.js
+++ client/src/component/auth/Register.js
@@ -3,8 +3,17 @@ import React, {Component} from 'react';
 class Register extends Component {
     render() {
         return (
-            <div>
-                <h1>Register</h1>
+            <div className="register">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <h1 className="display-4 text-center">Sign Up</h1>
+                            <p className={"lead text-center"}>
+                                Contact: Contact@CAEService.com for information of registerrring
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
