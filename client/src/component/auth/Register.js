5db878e43dda16878e4fc904c0e0b2b5272616e0
OH <ohmydaniel@gmail.com>
2020-08-26 00:54:19 +0900

    Regis&&Login

--- client/src/component/auth/Register.js
+++ client/src/component/auth/Register.js
@@ -1,6 +1,29 @@
 import React, {Component} from 'react';
 
 class Register extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            password2: '',
+            errors: {}
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+
+    }
+
     render() {
         return (
             <div className="register">
@@ -11,6 +34,60 @@ class Register extends Component {
                             <p className={"lead text-center"}>
                                 Contact: Contact@CAEService.com for information of registerrring
                             </p>
+
+                            <form onSubmit={this.onSubmit}>
+
+                                <div className="form-group">
+                                    <input
+                                        type={"text"}
+                                        className={"form-control form-control-lg"}
+                                        placeholder="Name"
+                                        name="name"
+                                        value={this.state.name}
+                                        onChange={this.onChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="email"
+                                        className={"form-control form-control-lg"}
+                                        placeholder="Email"
+                                        name="email"
+                                        value={this.state.email}
+                                        onChange={this.onChange}
+                                    />
+                                    <small className="form-text text-muted">
+                                        This site uses Gravatar so if you want a profile image,
+                                        use a Gravatar email
+                                    </small>
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type={"password"}
+                                        className={"form-control form-control-lg"}
+                                        placeholder="Password"
+                                        name={"password"}
+                                        value={this.state.password}
+                                        onChange={this.onChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type={"password"}
+                                        className={"form-control form-control-lg"}
+                                        placeholder="Confirm Password"
+                                        name="password2"
+                                        value={this.state.password2}
+                                        onChange={this.onChange}
+                                    />
+                                </div>
+                                <input
+                                    type="submit"
+                                    className="btn btn-info btn-block mt-4"
+                                />
+
+
+                            </form>
                         </div>
                     </div>
                 </div>
