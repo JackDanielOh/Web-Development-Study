d0e7a473f3dd260af9bd4947d7775c4aba24dd78
OH <ohmydaniel@gmail.com>
2020-09-01 23:27:47 +0900

    log and logout navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions'
+
+
+
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -15,6 +21,22 @@ import Login from "./component/auth/Login";
 
 
 import './App.css';
+
+// check for token
+if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+    const decoded = jwt_decode(localStorage.jwtToken);
+    store.dispatch(setCurrentUser(decoded));
+
+    const currentTime = Date.now() / 1000;
+    if (decoded.exp < currentTime) {
+        store.dispatch(logoutUser());
+        window.location.href = '/login';
+    }
+}
+
+
+
 
 function App() {
     return (
