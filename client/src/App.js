9a2d4a61857f66b9709591fc3d6fdc97990a81db
OH <ohmydaniel@gmail.com>
2020-09-03 23:24:30 +0900

    Private route setup (Xbrowse when no login)

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
@@ -11,6 +11,7 @@ import { setCurrentUser, logoutUser } from './actions/authActions'
 import { Provider } from 'react-redux';
 import store from './store';
 
+import PrivateRoute from "./component/common/PrivateRoute";
 
 import Navbar from "./component/layout/Navbar";
 import Landing from "./component/layout/Landing";
@@ -38,23 +39,31 @@ if (localStorage.jwtToken) {
 
 
 
-function App() {
-    return (
-        <Provider store={store}>
-            <Router>
-                <div className="App">
-                    <Navbar />
-                    <Route exact path="/" component={Landing} />
-                    <div className="container">
-                        <Route exact path="/register" component={Register} />
-                        <Route exact path="/login" component={Login} />
-                        <Route excat path="/dashboard" component={Dashboard} />
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <div className="App">
+                        <Navbar />
+                        <Route exact path="/" component={Landing} />
+                        <div className="container">
+                            <Route exact path="/register" component={Register} />
+                            <Route exact path="/login" component={Login} />
+                            <Switch>
+                                <PrivateRoute
+                                    exact
+                                    path="/dashboard"
+                                    component={Dashboard}
+                                />
+                            </Switch>
+                        </div>
+                        <Footer />
                     </div>
-                    <Footer />
-                </div>
-            </Router>
-        </Provider>
-    );
+                </Router>
+            </Provider>
+        );
+    }
 }
 
 export default App;
