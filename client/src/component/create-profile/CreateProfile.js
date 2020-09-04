ea42980c9ebc128c45f1dd11505f541c9caea64c
OH <ohmydaniel@gmail.com>
2020-09-04 12:45:04 +0900

    send create profile data

--- client/src/component/create-profile/CreateProfile.js
+++ client/src/component/create-profile/CreateProfile.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import InputGroup from "../common/InputGroup";
 import SelectListGroup from "../common/SelectListGroup";
+import { createProfile } from "../../actions/profileActions";
 
 class CreateProfile extends Component {
     constructor(props) {
@@ -31,12 +33,37 @@ class CreateProfile extends Component {
         this.onSubmit = this.onSubmit.bind(this);
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({errors: nextProps.errors})
+        }
+    }
+
     onSubmit(e) {
         e.preventDefault();
+
+        const profileData = {
+            handle: this.state.handle,
+            company: this.state.company,
+            website: this.state.website,
+            location: this.state.location,
+            status: this.state.status,
+            skills: this.state.skills,
+            githuberusername: this.state.githubusername,
+            bio: this.state.bio,
+            twitter: this.state.twitter,
+            facebook: this.state.facebook,
+            linkedin: this.state.linkedin,
+            youtube: this.state.youtube,
+            instagram: this.state.instagram
+
+        };
+        console.log(profileData);
+        this.props.createProfile(profileData, this.props.history);
     }
 
     onChange(e) {
-        this.setState({[e.tager.name]: e.target.value })
+        this.setState({[e.target.name]: e.target.value })
     }
 
     render() {
@@ -125,10 +152,10 @@ class CreateProfile extends Component {
                                 <TextFieldGroup
                                     placeholder="* Profile Handle"
                                     name="handle"
-                                    onChange={this.onChange}
                                     value={this.state.handle}
+                                    onChange={this.onChange}
                                     error={errors.handle}
-                                    info="A unique handle for your profile URL.Your full name, company name, nickname"
+                                    info="A unique handle for your profile URL. Your full name, company name, nickname"
                                 />
 
                                 <SelectListGroup
@@ -236,4 +263,6 @@ const mapStateToProps = state => ({
     errors: state.errors
 })
 
-export default connect(null)(CreateProfile);
+export default connect(mapStateToProps, { createProfile })(
+    withRouter(CreateProfile)
+);
